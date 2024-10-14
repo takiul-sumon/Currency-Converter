0bd9af3decc1fddf5104c28579d71a556e3bd720
Takiul ISlam Sumon <takiulislam20@gmail.com>
2024-10-14 13:20:24 +0600

    update

--- lib/currency_converter.dart
+++ lib/currency_converter.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
 
 class Currencyconverter extends StatefulWidget {
+  
   @override
   State<Currencyconverter> createState() => _CurrencyconverterState();
 }
@@ -22,46 +23,49 @@ class _CurrencyconverterState extends State<Currencyconverter> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Currency Converter',style: TextStyle(fontSize: 16,fontWeight: FontWeight.w800,color: Colors.white),),centerTitle: true,backgroundColor: Colors.blueGrey,),
-      
+      appBar: AppBar(
+        title: const Text(
+          'Currency Converter',
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.w800, color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+      ),
       backgroundColor: Colors.blueGrey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              amount.toString(),
+              'BDT ${amount!=0 ? amount.toStringAsFixed(2) : amount.toStringAsFixed(0)}',
               style: const TextStyle(
                   color: Colors.white,
                   fontSize: 40,
                   fontWeight: FontWeight.w600),
             ),
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding:  const EdgeInsets.all(8.0),
               child: TextField(
                 controller: textEditingController,
                 keyboardType: TextInputType.number,
                 decoration: const InputDecoration(
                     hintText: 'Enter Amount in USD',
                     hintStyle: TextStyle(color: Colors.black),
-                    // suffixIcon: Icon(Icons.money)
+                    
                     prefixIcon: Icon(
                       Icons.monetization_on_outlined,
                       color: Colors.black,
                     ),
-                    // focusedBorder: OutlineInputBorder(
-                    //     borderSide: BorderSide(color: Colors.black),
-                    //     borderRadius: BorderRadius.all(Radius.circular(5)),
-                    //     ),
+                    
                     filled: true,
                     fillColor: Colors.white,
                     border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black,
-                      width: 10.0,
-                      style: BorderStyle.solid
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(10))
-                    )),
+                        borderSide: BorderSide(
+                            color: Colors.black,
+                            width: 10.0,
+                            style: BorderStyle.solid),
+                        borderRadius: BorderRadius.all(Radius.circular(10)))),
               ),
             ),
             Padding(
@@ -74,15 +78,11 @@ class _CurrencyconverterState extends State<Currencyconverter> {
                     amount = amount * 120;
                   });
                 },
-                
                 style: TextButton.styleFrom(
                     backgroundColor: Colors.black,
                     shape: const RoundedRectangleBorder(
                         borderRadius: BorderRadius.all(Radius.circular(3))),
                     fixedSize: Size(480, 45)),
-
-                
-
                 child: const Text(
                   'Convert',
                   style: TextStyle(color: Colors.white),
